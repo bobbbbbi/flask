9ebb96291108ff0365dabf2a07bc7a7ced4280ee
salvador <salvav1@uci.edu>
2018-07-21 09:29:01 -0400

    UPDATED FILES

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+# import sll
+
+app = application = Flask(__name__)
+
+@app.route("/")
+def home():
+#    return "Hello World!"
+    return render_template("home.html")
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    # BIND to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
